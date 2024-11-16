7664b5f41b3cbcb483e8ab21e0cba929e0d384b8
fake-satan101 <lalligah@gmail.com>
2024-11-17 00:46:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function sound(button, audio, image){
 
 
 sound("w", "tom-1.mp3","./images/tom1.png")
-sound("a", "tom-2.mp3","tom2.png")
-sound("d", "tom-3.mp3","tom3.png")
-sound("s", "tom-4.mp3","tom4.png")
-sound("j", "crash.mp3","crash.png")
-sound("k", "snare.mp3","snare.png")
-sound("l", "kick-bass.mp3","kick.png")
+sound("a", "tom-2.mp3","./images/tom2.png")
+sound("d", "tom-3.mp3","./images/tom3.png")
+sound("s", "tom-4.mp3","./images/tom4.png")
+sound("j", "crash.mp3","./images/crash.png")
+sound("k", "snare.mp3","./images/snare.png")
+sound("l", "kick-bass.mp3","./images/kick.png")
